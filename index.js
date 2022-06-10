6c50608e3c0fb9bf68181b9e93786278bcaf0436
Elijah <bundielly@gmail.com>
2022-06-10 12:02:15 +0300

    Filtering done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+//Code your solution here
+
+function findMatching(array, name) {
+    const upperName = name.toUpperCase();
+    let match = array.filter((x) => {
+        return (x.toUpperCase() === upperName)
+    })
+    return match
+}
+
+function fuzzyMatch(array, letters) {
+    const newNames = array.filter((x) => {
+        return x.startsWith(letters)
+    })
+    return newNames
+}
+
+function matchName(array, string) {
+    const matchMaker = array.filter((x) => {
+        return (x.name === string)
+    })
+    return matchMaker
+}
+
+
+
+
+
+
+
